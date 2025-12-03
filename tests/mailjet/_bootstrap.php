b9943cd20ae3045db364a4af47ba75e8a5e114a1
Philippe Gaultier <pgaultier@gmail.com>
2025-12-03 16:22:36 +0100

    remove useless defines

--- tests/mailjet/_bootstrap.php
+++ tests/mailjet/_bootstrap.php
@@ -1,10 +1,3 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 
-define('MAILJET_FROM', '<sender>');
-define('MAILJET_KEY', '<key>');
-define('MAILJET_SECRET', '<secret>');
-define('MAILJET_TO', '<target>');
-define('MAILJET_TEMPLATE', 218932);
-
-define('MAILJET_TEST_SEND', false);
--- tests/sendgrid/_bootstrap.php
+++ tests/sendgrid/_bootstrap.php
@@ -1,9 +1,2 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-
-define('SENDGRID_FROM', '<FROM>');
-define('SENDGRID_TOKEN', '<SENDGRID_TOKEN>');
-define('SENDGRID_TO', '<TO>');
-define('SENDGRID_TEMPLATE', '<SENDGRID_TEST_TEMPLATE_ID>');
-
-define('SENDGRID_TEST_SEND', false);
